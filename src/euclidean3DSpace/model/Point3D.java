8ff1ab657196dd972c1ad3b755e33f5e83f55509
Tosh At Linux <tosh@example.com>
2020-11-09 19:49:14 +0200

    add comment and link

--- src/euclidean3DSpace/model/Point3D.java
+++ src/euclidean3DSpace/model/Point3D.java
@@ -1,4 +1,5 @@
 package euclidean3DSpace.model;
 
-public class Point3D {
+public class Point3D {  //Unlike C#, Java do NOT provide structures for now! It will be available in near future. More about "Java Inline Classes" --> https://www.infoq.com/articles/inline-classes-java/
+
 }
