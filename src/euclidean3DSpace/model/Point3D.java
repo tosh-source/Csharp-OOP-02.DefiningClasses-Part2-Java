8fb4a69a8af895857e17179d9537112c7e095bc4
Tosh At Linux <tosh@example.com>
2020-11-10 22:52:01 +0200

    Override toString() and add empty constructor.

--- src/euclidean3DSpace/model/Point3D.java
+++ src/euclidean3DSpace/model/Point3D.java
@@ -1,5 +1,7 @@
 package euclidean3DSpace.model;
 
+import java.text.MessageFormat;
+
 public class Point3D {  //Unlike C#, Java do NOT provide structures for now! It will be available in near future. More about "Java Inline Classes" --> https://www.infoq.com/articles/inline-classes-java/
 
     private static final Point3D startCoordinate;  //There is MO "static readonly" in Java! NOTE: "final" is like "readonly" field on C#. "static final" is like "const" field on C#.
@@ -35,9 +37,21 @@ public class Point3D {  //Unlike C#, Java do NOT provide structures for now! It 
     static {
         startCoordinate = new Point3D(0d, 0d, 0d);
     }
+    public Point3D() {
+    }
     public Point3D(double x, double y, double z) {
+        this();
         this.x = x;
         this.y = y;
         this.z = z;
+    }
+
+    @Override
+    public String toString() {
+        var result = "";
+        result += String.format(MessageFormat
+                .format("X:{0}, Y:(1), Z:{2}", getX(), getY(), getZ()));
+
+        return result;
     }
 }
