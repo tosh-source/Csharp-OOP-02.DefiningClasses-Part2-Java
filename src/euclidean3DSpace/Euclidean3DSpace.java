c24c23350e3e84186f6d209612b807cf0ce8bc01
Tosh At Linux <tosh@example.com>
2020-11-12 16:01:10 +0200

    work on Euclidean3DSpace class

--- src/euclidean3DSpace/Euclidean3DSpace.java
+++ src/euclidean3DSpace/Euclidean3DSpace.java
@@ -1,4 +1,14 @@
 package euclidean3DSpace;
 
+import euclidean3DSpace.model.Point3D;
+
 public class Euclidean3DSpace {
+    public static void main(String[] args) {
+        //region TESTS
+        //Test point3D and calculation
+        Point3D point3D = new Point3D();
+
+        //endregion
+
+    }
 }
