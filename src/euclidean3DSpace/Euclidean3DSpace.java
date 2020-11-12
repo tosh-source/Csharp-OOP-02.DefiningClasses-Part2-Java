7146ef96494e537b3e2de43d82f8711546fff98e
Tosh At Linux <tosh@example.com>
2020-11-12 23:28:22 +0200

    work on Euclidean3DSpace class

--- src/euclidean3DSpace/Euclidean3DSpace.java
+++ src/euclidean3DSpace/Euclidean3DSpace.java
@@ -1,14 +1,17 @@
 package euclidean3DSpace;
 
+import euclidean3DSpace.extensions.ExtensionPointsOf3DSpace;
 import euclidean3DSpace.model.Point3D;
 
 public class Euclidean3DSpace {
     public static void main(String[] args) {
         //region TESTS
         //Test point3D and calculation
-        Point3D point3D = new Point3D();
+        Point3D point3D = new Point3D(1, 2, 3);
+        System.out.println(point3D);
+        System.out.println(Point3D.getStartCoordinate());
+        System.out.println(ExtensionPointsOf3DSpace.calculateDistance(point3D, Point3D.getStartCoordinate()));
 
         //endregion
-
     }
 }
