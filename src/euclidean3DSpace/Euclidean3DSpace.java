83c8ad94ba5b4f162af3477829f3c4de7acde304
Tosh At Linux <tosh@example.com>
2020-11-15 01:29:31 +0200

    add more tests

--- src/euclidean3DSpace/Euclidean3DSpace.java
+++ src/euclidean3DSpace/Euclidean3DSpace.java
@@ -1,6 +1,7 @@
 package euclidean3DSpace;
 
 import euclidean3DSpace.extensions.ExtensionPointsOf3DSpace;
+import euclidean3DSpace.model.Path;
 import euclidean3DSpace.model.Point3D;
 
 public class Euclidean3DSpace {
@@ -12,6 +13,14 @@ public class Euclidean3DSpace {
         System.out.println(Point3D.getStartCoordinate());
         System.out.println(ExtensionPointsOf3DSpace.calculateDistance(point3D, Point3D.getStartCoordinate()));
 
+        //Test collection of points (aka. Path)
+        Path points = new Path();
+        points.addPoint(point3D);
+        points.addPoint(Point3D.getStartCoordinate());
+        points.addPoint(new Point3D(3, 3, 3));
+//        for (var item : points) {
+//
+//        }
         //endregion
     }
 }
