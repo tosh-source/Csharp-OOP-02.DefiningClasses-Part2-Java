428004902eda32f81713c117acdfb2802d277b1c
Tosh At Linux <tosh@example.com>
2020-11-15 00:33:08 +0200

    complete Path class

--- src/euclidean3DSpace/model/Path.java
+++ src/euclidean3DSpace/model/Path.java
@@ -1,4 +1,23 @@
 package euclidean3DSpace.model;
 
+import java.util.AbstractCollection;
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Path {
+
+    private AbstractCollection<Point3D> points;  //This class provides a skeletal implementation of the Collection interface. **
+
+    public void addPoint(Point3D point){
+        this.points.add(point);
+    }
+    public void removePoint(Point3D point){
+        this.points.remove(point);
+    }
+
+    public Path(){
+        this.points = new ArrayList<>();
+    }
 }
+
+// ** more info --> https://docs.oracle.com/javase/7/docs/api/java/util/AbstractCollection.html
