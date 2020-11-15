eb68ae64ec5fbe2bb239dbcb776f9cbb8cc81df2
Tosh At Linux <tosh@example.com>
2020-11-15 02:22:12 +0200

    implement Java Iterator

--- src/euclidean3DSpace/model/Path.java
+++ src/euclidean3DSpace/model/Path.java
@@ -2,9 +2,9 @@ package euclidean3DSpace.model;
 
 import java.util.AbstractCollection;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Iterator;
 
-public class Path {
+public class Path implements Iterable<Point3D> {
 
     private AbstractCollection<Point3D> points;  //This class provides a skeletal implementation of the Collection interface. **
 
@@ -18,6 +18,13 @@ public class Path {
     public Path(){
         this.points = new ArrayList<>();
     }
+
+    //Implement iterator() to use "foreach" loop. NOTE: Implementation of "foreach" METHOD is another option to iterate collection.
+    @Override
+    public Iterator<Point3D> iterator() {
+        return this.points.iterator();
+    }
 }
 
 // ** more info --> https://docs.oracle.com/javase/7/docs/api/java/util/AbstractCollection.html
+//How to implement iterator()  --> https://stackoverflow.com/questions/975383/how-can-i-use-the-java-for-each-loop-with-custom-classes
