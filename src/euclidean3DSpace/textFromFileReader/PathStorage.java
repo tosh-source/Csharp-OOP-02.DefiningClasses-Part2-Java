9da9062a1b830fd1f3a744d8489808fff24caa0c
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2020-11-27 23:09:58 +0200

    work on PathStorage

--- src/euclidean3DSpace/textFromFileReader/PathStorage.java
+++ src/euclidean3DSpace/textFromFileReader/PathStorage.java
@@ -1,4 +1,25 @@
 package euclidean3DSpace.textFromFileReader;
 
-public class PathStorage {
+import euclidean3DSpace.model.Path;
+
+import java.io.File;
+import java.util.Scanner;
+
+public final class PathStorage {
+    public static Path LoadPath(String directory, String fileName) {
+        //Method can read Point3D in format "X = 1, Y = 2, Z = 3" or "X:1 Y:2 Z:3". There is no limit how many Point3D can be read as collection (Models.Path). Every Point3D need to be in NEW line!
+
+        Path collectionOfPoints = new Path();
+            try{
+                File file = new File(directory + fileName);
+                Scanner reader = new Scanner(file);
+                while (reader.hasNextLine()){
+
+                }
+
+
+            } catch (Exception e) {
+                e.getStackTrace();
+            }
+    }
 }
