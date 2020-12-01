1de97738360843fe1dad2ad602f8ce852d5a4b53
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2020-12-02 00:08:41 +0200

    work on method LoadPath

--- src/euclidean3DSpace/textFromFileReader/PathStorage.java
+++ src/euclidean3DSpace/textFromFileReader/PathStorage.java
@@ -1,6 +1,7 @@
 package euclidean3DSpace.textFromFileReader;
 
 import euclidean3DSpace.model.Path;
+import euclidean3DSpace.model.Point3D;
 
 import java.io.File;
 import java.util.Scanner;
@@ -10,16 +11,30 @@ public final class PathStorage {
         //Method can read Point3D in format "X = 1, Y = 2, Z = 3" or "X:1 Y:2 Z:3". There is no limit how many Point3D can be read as collection (Models.Path). Every Point3D need to be in NEW line!
 
         Path collectionOfPoints = new Path();
-            try{
-                File file = new File(directory + fileName);
-                Scanner reader = new Scanner(file);
-                while (reader.hasNextLine()){
+        try {
+            File file = new File(directory + fileName);
+            Scanner reader = new Scanner(file);
 
+            String currentLine;
+            while (reader.hasNextLine()) {
+                currentLine = reader.nextLine();
+
+                String[] splittedText = currentLine.trim()  //TO DO: add "\\s+" to RemoveEmptyEntries
+                        .split("X = |,| |Y = |Z = |X:|X: |Y:|Y: |Z:|Z: ");  //Use Regular expression OR "|" , to catch many strings. Example --> "ABC|abc|ONE|two"
+
+                Double[] XYZ_points = new Double[3];  //no more 3 elements needed
+                for (int index = 0; index <= splittedText.length - 1; index++) {
+                    XYZ_points[index] = Double.parseDouble(splittedText[index]);  //if more than three doubles, IndexOutOfRangeException
                 }
 
-
-            } catch (Exception e) {
-                e.getStackTrace();
+                Point3D currentPoint = new Point3D(XYZ_points[0], XYZ_points[1], XYZ_points[2]);
+                collectionOfPoints.addPoint(currentPoint);
             }
+
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+        return collectionOfPoints;
     }
+
 }
