c2f0d269e89b90b032d4e214f69dbdf9d0cece98
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2020-12-03 00:38:08 +0200

    work on LoadPath method

--- src/euclidean3DSpace/textFromFileReader/PathStorage.java
+++ src/euclidean3DSpace/textFromFileReader/PathStorage.java
@@ -4,6 +4,9 @@ import euclidean3DSpace.model.Path;
 import euclidean3DSpace.model.Point3D;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public final class PathStorage {
@@ -12,15 +15,23 @@ public final class PathStorage {
 
         Path collectionOfPoints = new Path();
         try {
-            File file = new File(directory + fileName);
-            Scanner reader = new Scanner(file);
+            var file = new File(directory + fileName);
+            var reader = new Scanner(file);
 
             String currentLine;
             while (reader.hasNextLine()) {
                 currentLine = reader.nextLine();
 
-                String[] splittedText = currentLine.trim()  //TO DO: add "\\s+" to RemoveEmptyEntries
-                        .split("X = |,| |Y = |Z = |X:|X: |Y:|Y: |Z:|Z: ");  //Use Regular expression OR "|" , to catch many strings. Example --> "ABC|abc|ONE|two"
+                String[] splittedText = currentLine.split("X = |,| |Y = |Z = |X:|X: |Y:|Y: |Z:|Z: ");  //Use Regular expression OR "|" , to catch many strings. Example --> "ABC|abc|ONE|two"
+
+                //Remove empty entries from result
+                var tempVal = new ArrayList<>();
+                for (var item : splittedText) {
+                    if (!(item.isEmpty())){   //add only if there is a value
+                        tempVal.add(item);
+                    }
+                }
+                splittedText = tempVal.toArray(String[]::new);
 
                 Double[] XYZ_points = new Double[3];  //no more 3 elements needed
                 for (int index = 0; index <= splittedText.length - 1; index++) {
