030856a9050ae5341fa2a3c401beb3f5ffbd508b
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2020-12-09 21:45:22 +0200

    work on GenericList

--- src/genericClasses/models/GenericList.java
+++ src/genericClasses/models/GenericList.java
@@ -1,4 +1,28 @@
 package genericClasses.models;
 
-public class GenericList {
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class GenericList<T> implements Iterable<T> {
+
+    private T[] elements;
+    private Integer currentPosition = 0;
+
+    public T[] getElements() {
+        return this.elements;
+    }
+
+    public void setElements(T[] elements) {
+        this.elements = elements;
+    }
+
+    public GenericList(T[] elements) {
+        this.elements = elements;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return Arrays.stream(this.getElements()).iterator();
+    }
+
 }
