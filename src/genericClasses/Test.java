7b0d15a1cd15a4e219e3597532cf4140a3d5dfa4
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-04-02 01:44:11 +0300

    add test

--- src/genericClasses/Test.java
+++ src/genericClasses/Test.java
@@ -1,4 +1,11 @@
 package genericClasses;
 
+import genericClasses.models.GenericList;
+
 public class Test {
+    public static void main(String[] args) {
+
+        GenericList<Integer> genericList = new GenericList<Integer>();
+
+    }
 }
