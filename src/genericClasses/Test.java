9a5502cc315cfca4fb621cdd012aab49ea5d5164
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-04-05 18:45:16 +0300

    add more tests

--- src/genericClasses/Test.java
+++ src/genericClasses/Test.java
@@ -5,7 +5,16 @@ import genericClasses.models.GenericList;
 public class Test {
     public static void main(String[] args) {
 
-        GenericList<Integer> genericList = new GenericList<Integer>();
+        GenericList<Integer> genericInts = new GenericList<Integer>();
+        genericInts.Add(5);
+        genericInts.Add(10);
+        genericInts.Add(20);
+
+
+        for (var genericInt : genericInts) {
+            var tmpStr = genericInt != null ? genericInt.toString() : "";  //Generic array can be initialized, but can be null (have no value). This will prevent exception.
+            System.out.println(String.join("", tmpStr));
+        }
 
     }
 }
