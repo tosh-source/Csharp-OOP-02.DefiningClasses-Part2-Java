732d79ee945ec895ffc399d1b16cdb2f15b908a1
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-04-14 00:44:26 +0300

    add more tests

--- src/genericClasses/Test.java
+++ src/genericClasses/Test.java
@@ -5,15 +5,26 @@ import genericClasses.models.GenericList;
 public class Test {
     public static void main(String[] args) {
 
-        GenericList<Integer> genericInts = new GenericList<Integer>();
+        //Create new Generic List of Integers and add some items.
+        var genericInts = new GenericList<Integer>(3);
         genericInts.Add(5);
         genericInts.Add(10);
         genericInts.Add(20);
+        System.out.println(genericInts);
 
+        //for (var genericInt : genericInts) {  //This will show null values too.
+        //    System.out.println(genericInt);
+        //}
 
-        for (var genericInt : genericInts) {
-            System.out.println(genericInt);
-        }
+        //Create new Generic List of Strings and add some items.
+        var genericStrings = new GenericList<String>(4);
+        genericStrings.Add("aaa");
+        genericStrings.Add("bbb");
+        genericStrings.Add("ccc");
+        System.out.println(genericStrings);
 
+        //Test Remove method.
+        genericInts.Remove(2);
+        System.out.println(genericInts);
     }
 }
